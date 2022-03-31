ebf720d7cc78a7a65998b3f8ac0565786c75ad52
Eric Carlsen <carlsen.eric@gmail.com>
2022-03-31 14:31:08 -0600

    Make a function to call from elsewhere

--- python/plot_caf.py
+++ python/plot_caf.py
@@ -5,19 +5,24 @@ import numpy as np
 import bluefile as bf
 import sys
 
-infile = sys.argv[1]
+def plot_caf(infile):
 
-bf.set_type2000_format(np.ndarray)
-h, d = bf.read(infile)
-fdoa = np.arange(len(d[0]) + 1) * h['xdelta'] + h['xstart']
-tdoa = np.arange(len(d) + 1) * h['ydelta'] + h['ystart']
+    bf.set_type2000_format(np.ndarray)
+    h, d = bf.read(infile)
+    fdoa = np.arange(len(d[0]) + 1) * h['xdelta'] + h['xstart']
+    tdoa = np.arange(len(d) + 1) * h['ydelta'] + h['ystart']
 
-cmap = plt.colormaps['gray']
-plt.grid(False)
+    cmap = plt.colormaps['inferno']
+    plt.grid(False)
 
-#  plt.pcolormesh(fdoa, 1e3 * tdoa, np.abs(d), shading='flat', cmap=cmap)
-plt.pcolor(fdoa, 1e3 * tdoa, np.abs(d), shading='flat', cmap=cmap)
-plt.xlabel('FDOA (Hz)')
-plt.ylabel('TDOA (ms)')
+    #  plt.pcolormesh(fdoa, 1e3 * tdoa, np.abs(d), shading='flat', cmap=cmap)
+    plt.pcolor(fdoa, 1e3 * tdoa, np.abs(d), shading='flat', cmap=cmap)
+    plt.xlabel('FDOA (Hz)')
+    plt.ylabel('TDOA (ms)')
+    plt.title('Cross-Ambiguity Function')
 
-plt.show()
+    plt.show()
+
+if __name__ == "__main__":
+    infile = sys.argv[1]
+    plot_caf(infile)
