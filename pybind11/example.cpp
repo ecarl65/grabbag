f3c879939fe27647611e60bb0775b7ce13dd3c71
Eric Carlsen <ecarlsen@icr-team.com>
2022-04-25 12:20:48 -0600

    Defaults and clean

--- pybind11/example.cpp
+++ pybind11/example.cpp
@@ -22,12 +22,16 @@ namespace py = pybind11;
 
 #include <pybind11/pybind11.h>
 
-int add(int i, int j) {
+int add(int i=1, int j=2) {
     return i + j;
 }
 
 PYBIND11_MODULE(example, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
-    m.def("add", &add, "A function that adds two numbers");
+    m.def("add1", &add, "A function that adds two numbers",
+        py::arg("i") = 1, py::arg("j") = 2);
+
+    using namespace pybind11::literals;
+    m.def("add2", &add, "A function that adds two numbers", "i"_a = 1, "j"_a = 2);
 }
