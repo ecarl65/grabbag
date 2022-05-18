7c41a35e7dabfac020e32686b45318cd24ab33ee
Eric Carlsen <ecarlsen@icr-team.com>
2022-05-18 09:01:11 -0600

    Colorized mandelbrot

---
+++ python/mandel_blue.py
@@ -0,0 +1,33 @@
+
+def mandel(data):
+    for ii in range(1000):
+        im = (ii - 500)*.002
+        for jj in range(1500):
+            re = (jj - 1000)*.002
+            xx = re
+            yy = im
+            cc = 0
+            while 1:
+                tx = xx*xx - yy*yy + re
+                ty = 2*xx*yy + im
+                r2 = tx*tx + ty*ty
+                xx = tx
+                yy = ty
+                cc += 1
+                if r2 > 4.0 or cc == 255:
+                    data[ii*1500 + jj] = cc
+                    break
+
+
+if __name__ == '__main__':
+    from os import write
+    data = bytearray(1500*1000)
+    mandel(data)
+    datafull = bytearray(3*1500*1000)
+
+    for x in range(1500*1000):
+        datafull[3*x] = 0
+        datafull[3*x+1] = 0
+        datafull[3*x+2] = 255 - data[x]
+    write(1, b"P6 1500 1000 255\n")
+    write(1, datafull)
