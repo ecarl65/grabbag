f65f9736064b7579c8ab890875f1bf32119d2300
Eric Carlsen <ecarlsen@icr-team.com>
2022-05-31 11:09:49 -0600

    Plot one row of output, get indices right

--- c/plot_fftw.py
+++ c/plot_fftw.py
@@ -8,16 +8,21 @@ indata = np.reshape(np.fromfile("twodin.bin", dtype=np.float32), (32, -1));
 outdata = np.reshape(np.fromfile("twodout.bin", dtype=np.complex64), (32, -1));
 
 fs = 500
-fig, axs = plt.subplots(2)
+fig, axs = plt.subplots(3)
 im0 = axs[0].pcolormesh(indata)
 axs[0].set_title("Input Data")
-f = (np.arange(outdata.shape[1] + 1) - 0.5) / outdata.shape[1] * fs
-c = (np.arange(outdata.shape[0] + 1) - 0.5)
-im1 = axs[1].pcolormesh(f, c, 20*np.log10(np.abs(outdata) + 1e-6))
+f = np.arange(outdata.shape[1]) / outdata.shape[1] * fs
+c = np.arange(outdata.shape[0])
+im1 = axs[1].pcolormesh(f, c, 20*np.log10(np.abs(outdata) + 1e-6), shading="nearest")
 
 axs[1].set_title("Rows FFT Output")
-axs[1].set_xlabel("Frequency (index)")
+axs[1].set_xlabel("Frequency (Hz)")
 axs[1].set_ylabel("Magnitude (dB)")
+
+axs[2].plot(f, np.abs(outdata[-1, :]))
+axs[2].set_xlabel("Frequency (Hz)")
+axs[2].set_ylabel("FFT Output Magnitude")
+axs[2].set_title("15.5 Hz Row")
 
 fig.colorbar(im0, ax=axs[0])
 fig.colorbar(im1, ax=axs[1])
