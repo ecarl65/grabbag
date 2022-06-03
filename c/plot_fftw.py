3c6bbb497edfe92fb0703da3bc820ed6fb168a1f
Eric Carlsen <ecarlsen@icr-team.com>
2022-06-03 15:29:05 -0600

    Reduce warnings

--- c/plot_fftw.py
+++ c/plot_fftw.py
@@ -8,6 +8,8 @@ indata = np.reshape(np.fromfile("twodin.bin", dtype=np.float32), (32, -1))
 outdata = np.reshape(np.fromfile("twodout.bin", dtype=np.complex64), (32, -1))
 oned = np.fromfile("oned.bin", dtype=np.complex64)
 
+plt.rcParams["axes.grid"] = False
+
 fs = 500
 fig, axs = plt.subplots(2, 2)
 f1 = np.linspace(0, 0.5, len(oned), endpoint=False)
@@ -15,14 +17,13 @@ axs[0, 0].plot(f1, 20 * np.log10(np.abs(oned)))
 axs[0, 0].set_title("0.05 Peak")
 axs[0, 0].set_xlabel("Norm. Freq")
 axs[0, 0].set_ylabel("Magnitude")
+axs[0, 0].grid(True)
 
-axs[0, 1].grid(False)
 im0 = axs[0, 1].pcolormesh(indata)
 axs[0, 1].set_title("Input Data")
 
 f = np.linspace(0, fs / 2, outdata.shape[1], endpoint=False)
 c = np.arange(outdata.shape[0])
-axs[1, 1].grid(False)
 im1 = axs[1, 1].pcolormesh(f, c, 20*np.log10(np.abs(outdata) + 1e-6), shading="nearest")
 
 axs[1, 1].set_title("Rows FFT Output")
@@ -33,10 +34,12 @@ axs[1, 0].plot(f, np.abs(outdata[-1, :]))
 axs[1, 0].set_xlabel("Frequency (Hz)")
 axs[1, 0].set_ylabel("FFT Output Magnitude")
 axs[1, 0].set_title("15.5 Hz Row")
+axs[1, 0].grid(True)
 
-#  fig.colorbar(im0, ax=axs[1])
-#  fig.colorbar(im1, ax=axs[2])
+fig.colorbar(im0, ax=axs[0, 1])
+fig.colorbar(im1, ax=axs[1, 1])
 
 plt.tight_layout()
+plt.rcParams["axes.grid"] = True
 
 plt.show()
