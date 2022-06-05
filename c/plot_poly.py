11dc60b3e8e2b24e9ba1f68330be6b655ca9f96f
Eric Carlsen <carlsen.eric@gmail.com>
2022-06-05 17:35:13 -0500

    Beggining work on polyphase decomp

--- c/plot_poly.py
+++ c/plot_poly.py
@@ -4,31 +4,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
+M = 4
+
 indata = np.fromfile("input.bin", dtype=np.float64)
 outdata = np.fromfile("filtered.bin", dtype=np.float64)
 dsdata = np.fromfile("downsampled.bin", dtype=np.float64)
 filt = np.fromfile("filter.bin", dtype=np.float64)
+fdata = np.reshape(np.fromfile("fftdata.bin", dtype=np.complex128), (M, -1))
+ffilt = np.reshape(np.fromfile("fftfilt.bin", dtype=np.complex128), (M, -1))
 
 fs = 10e3
-fig, axs = plt.subplots(3)
-axs[0].plot(filt)
-axs[0].set_title("Filter Taps")
+fig, axs = plt.subplots(3, 2)
+axs[0, 0].plot(filt)
+axs[0, 0].set_title("Filter Taps")
 tin = np.arange(len(indata)) / fs
 tout = np.arange(len(outdata)) / fs
 tdsout =  np.arange(len(dsdata)) / fs * 4
 w, h = signal.freqz(filt, 1, fs=10e3)
-axs[1].plot(w, 20*np.log10(np.abs(h)))
-axs[1].plot([100, 100], [-60, 0], "r:")
-axs[1].set_title("Filter Frequency Response")
-axs[1].set_xlabel("Frequency (Hz)")
-axs[1].set_ylabel("Magnitude (dB)")
-axs[2].plot(tin, indata, label="Input")
-axs[2].plot(tout, outdata, "r", label="O/S Filtered");
-axs[2].plot(tdsout, dsdata, "g.", label="Downsampled");
-axs[2].set_title("Input & Output")
-axs[2].set_xlabel("Time (s)")
-axs[2].set_ylabel("Amplitude")
-axs[2].legend()
+axs[1, 0].plot(w, 20*np.log10(np.abs(h)))
+axs[1, 0].plot([100, 100], [-60, 0], "r:")
+axs[1, 0].set_title("Filter Frequency Response")
+axs[1, 0].set_xlabel("Frequency (Hz)")
+axs[1, 0].set_ylabel("Magnitude (dB)")
+axs[0, 1].plot(tin, indata, label="Input")
+axs[0, 1].plot(tout, outdata, "r", label="O/S Filtered");
+axs[0, 1].plot(tdsout, dsdata, "g.", label="Downsampled");
+axs[0, 1].set_title("Input & Output")
+axs[0, 1].set_xlabel("Time (s)")
+axs[0, 1].set_ylabel("Amplitude")
+axs[0, 1].legend()
+
+axs[1, 1].pcolormesh(np.abs(ffilt))
+axs[1, 1].set_title("Filter Decomposition")
+axs[1, 1].set_xlabel("Frequency Index")
+axs[1, 1].set_ylabel("Polyphase Channel")
+
+axs[2, 1].pcolormesh(np.abs(fdata))
+axs[2, 1].set_title("FFT of Polyphase Data")
+axs[2, 1].set_xlabel("Frequency Index")
+axs[2, 1].set_ylabel("Polyphase Channel")
 
 plt.tight_layout()
 
--- c/poly.c
+++ c/poly.c
@@ -34,27 +34,35 @@ double coarse_gaussian() {
 }
 
 int main(int argc, char **argv) {
-  // Make input sinusoid
-  const int Nbuf = 1024;
-  const int Nbufs = 20;
-  const int Nfull = Nbuf * Nbufs;
+  // Variables
+  const int M = 4; // Downsample factor
+  const int Nfull = 128 * M;  // Total number of samples
+  const int Nds = Nfull / M;
+  const int Nfilt = 16 * M + 1;
+  const int Nfilt_half = (Nfilt - 1) >> 1;
   const double pi = acosf(-1);
   const double Fs = 10e3;
   const double Ts = 1.0 / Fs;
   const double fc = 100;
-  const double ff = 2 * fc;
-  double *full_in = (double *) fftw_malloc(sizeof(double) * Nfull);
+  const double ff = 2 * fc;  // Double the tone freq for the filter cutoff
+  const int nrows = M;
+  const int ncols = Nds;
+  const int ncols_fft = ((Nfull / M) / 2 + 1);
+  const int Nfft = ncols_fft * M;
   srand(time(NULL));
-  for (int m = 0; m < Nfull; m++) {
-    full_in[m] = cos(2 * pi * fc * m * Ts) + coarse_gaussian() / 10.0;
-  }
+
+  // Arrays
+  double *full_in = (double *) fftw_malloc(sizeof(double) * Nfull);
+  double filt[Nfilt];
+  double *filt_full = (double *) fftw_malloc(sizeof(double) * Nfull);
+  double *ds_out = fftw_malloc(sizeof(double) * Nds);
   double *full_out = fftw_malloc(sizeof(double) * Nfull);
-  memset(&full_out[0], 0, sizeof(full_out[0]) * Nfull);
+  fftw_complex *fft_in = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * Nfft);
+  fftw_complex *fft_filt = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * Nfft);
+  double *conv_out = (double *) fftw_malloc(sizeof(double) * Nfull);
+  fftw_complex *fft_mult = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * Nfft);
 
   // Make filter
-  const int Nfilt = 129;
-  const int Nfilt_half = (Nfilt - 1) >> 1;
-  double *filt = (double *) fftw_malloc(sizeof(double) * Nbuf);
   int outidx = 0;
   double filt_sum = 0;
   for (int m = -Nfilt_half; m <= Nfilt_half; m++) {
@@ -69,68 +77,88 @@ int main(int argc, char **argv) {
     filt[outidx] *= (0.54 - 0.46 * cos(2 * pi * outidx / Nfilt));
     filt_sum += filt[outidx++];
   }
-  for (int m = 0; m < Nbuf; m++) {
-    if (m < Nfilt) {
-      filt[m] /= filt_sum;
-    } else {
-      filt[m] = 0.0f;
+  for (int m = 0; m < Nfilt; m++) filt[m] /= filt_sum;  // Normalize the filter
+
+  // Make input sinusoid
+  for (int m = 0; m < Nfull; m++) {
+    full_in[m] = cos(2 * pi * fc * m * Ts) + coarse_gaussian() / 10.0;
+  }
+
+  // output
+  for (size_t m = 0; m < Nfull; m++) full_out[m] = 0;
+  for (size_t m = 0; m < Nds; m++) ds_out[m] = 0;
+
+  // Filter polyphase decomposition
+  for (int n = 0; n < ncols; n++) {
+    for (int rho = 0; rho < M; rho++) {
+      int inidx = n * M - rho;
+      int outidx = rho * ncols + n;
+      if (inidx < 0 || inidx >= Nfilt) {
+        filt_full[outidx] = 0;
+      } else {
+        filt_full[outidx] = filt[inidx];
+      }
+      outidx++;
     }
   }
-  fftw_complex *f_filt_out = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * Nbuf);
-  fftw_plan pfilt = fftw_plan_dft_r2c_1d(Nbuf, filt, f_filt_out, FFTW_ESTIMATE);
+
+  // Data polyphase FFT
+  int n_size[] = {ncols};
+  int rank = 1;
+  int howmany = M;
+  int idist = 1;
+  int odist = ncols_fft;
+  int istride = M;
+  int ostride = 1;
+  int *inembed = NULL;
+  int *onembed = NULL;
+  fftw_plan psig = fftw_plan_many_dft_r2c(rank, n_size, howmany, full_in, inembed, istride, idist, fft_in,
+                                            onembed, ostride, odist, FFTW_ESTIMATE);
+
+  // Filter FFT
+  n_size[0] = ncols;
+  rank = 1;
+  howmany = M;
+  idist = ncols;
+  odist = ncols_fft;
+  istride = 1;
+  ostride = 1;
+  inembed = NULL;
+  onembed = NULL;
+  fftw_plan pfilt = fftw_plan_many_dft_r2c(rank, n_size, howmany, filt_full, inembed, istride, idist, fft_filt,
+                                             onembed, ostride, odist, FFTW_ESTIMATE);
   fftw_execute(pfilt);
   
-  // FFT plans
-  double *buf = (double *) fftw_malloc(sizeof(double) * Nbuf);
-  fftw_complex *f_buf = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * Nbuf);
-  fftw_plan psig = fftw_plan_dft_r2c_1d(Nbuf, buf, f_buf, FFTW_ESTIMATE);
-  double *conv_out = (double *) fftw_malloc(sizeof(double) * Nbuf);
-  fftw_plan pinv = fftw_plan_dft_c2r_1d(Nbuf, f_buf, conv_out, FFTW_ESTIMATE);
+  // IFFT plan
+  n_size[0] = ncols_fft;
+  rank = 1;
+  howmany = M;
+  idist = ncols_fft;
+  odist = ncols;
+  istride = 1;
+  ostride = 1;
+  inembed = NULL;
+  onembed = NULL;
+  fftw_plan pinv = fftw_plan_many_dft_c2r(rank, n_size, howmany, fft_mult, inembed, istride, idist, conv_out,
+                                            onembed, ostride, odist, FFTW_ESTIMATE);
 
-  // TODO Use wisdom
+  // Forward FFT
+  fftw_execute(psig);
 
-  // Loop through number of buffers
-  const int nv = Nbuf - Nfilt + 1;
-  const int dstrt = Nfilt - 1;
-  const int num_loops = Nfull / nv;
-  const int delay = dstrt >> 1;
-  const int ds = 4;
-  const int Nds = Nfull / ds;
-  double *ds_out = fftw_malloc(sizeof(double) * Nds);
-  printf("Doing %d loops\n", num_loops);
-  for (int bn = 0; bn < num_loops; bn++) {
-    // Forward FFT
-    int strt = bn * nv;
-    fftw_execute_dft_r2c(psig, &full_in[strt], f_buf);
-
-    // Compute circular convolution
-    for (int m = 0; m < Nbuf; m++) {
-      f_buf[m] *= f_filt_out[m];
-    }
-    fftw_execute(pinv);
-    /* fftw_execute_dft_c2r(pinv, f_buf, conv_out); */
-
-    // Save only valid portion. Re-normalize inverse FFT.
-    for (int m = 0; m < nv; m++) {
-      int outidx = m + strt + delay;
-      full_out[outidx] = conv_out[m + dstrt] / Nbuf;
-      if (outidx % ds == 0) {
-        ds_out[outidx / ds] = full_out[outidx];
-      }
-    }
+  // Compute circular convolution
+  for (int m = 0; m < Nfft; m++) {
+    fft_mult[m] = fft_filt[m] * fft_in[m];
   }
+  fftw_execute(pinv);
 
-  // Perform downsampling
-  for (int m = 0; m < Nfull; m++) {
-    if (m % ds == 0) {
-      /* ds_out[m/ds] = full_out[m]; */
+  // Perform summation and get downsampled output. Also normalize inverse fft.
+  for (int m = 0; m < Nds; m++) {
+    for (int rho = 0; rho < M; rho++) {
+      ds_out[m] += full_out[m + rho * Nds] / Nfull;
     }
   }
 
   // Write outputs
-  FILE* dsout = fopen("downsampled.bin", "wb");
-  fwrite(&ds_out[0], sizeof(double), Nds, dsout);
-  fclose(dsout);
   FILE* dout = fopen("filtered.bin", "wb");
   fwrite(&full_out[0], sizeof(double), Nfull, dout);
   fclose(dout);
@@ -140,16 +168,25 @@ int main(int argc, char **argv) {
   FILE *f_filt = fopen("filter.bin", "wb");
   fwrite(&filt[0], sizeof(double), Nfilt, f_filt);
   fclose(f_filt);
+  FILE* dsout = fopen("downsampled.bin", "wb");
+  fwrite(&ds_out[0], sizeof(double), Nds, dsout);
+  fclose(dsout);
+  FILE* ddata = fopen("fftdata.bin", "wb");
+  fwrite(&fft_in[0], sizeof(fftw_complex), Nfft, ddata);
+  fclose(ddata);
+  FILE* dfilt = fopen("fftfilt.bin", "wb");
+  fwrite(&fft_filt[0], sizeof(fftw_complex), Nfft, dfilt);
+  fclose(dfilt);
 
   // Free memory
   fftw_free(full_in);
-  fftw_free(full_out);
+  fftw_free(filt_full);
   fftw_free(ds_out);
-  fftw_free(filt);
-  fftw_free(f_filt_out);
-  fftw_free(buf);
-  fftw_free(f_buf);
+  fftw_free(full_out);
+  fftw_free(fft_in);
+  fftw_free(fft_filt);
   fftw_free(conv_out);
+  fftw_free(fft_mult);
   fftw_destroy_plan(pfilt);
   fftw_destroy_plan(psig);
   fftw_destroy_plan(pinv);
