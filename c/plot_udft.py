a11b54aea0558798966e0e14372489ccf8a78ff9
Eric Carlsen <ecarlsen@icr-team.com>
2022-06-06 12:30:41 -0600

    First version of UDFT almost working - just issues with real FFT at the end and plotting/array sizes

---
+++ c/plot_udft.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+M = 4
+
+indata = np.fromfile("input.bin", dtype=np.float64)
+outdata = np.fromfile("filtered.bin", dtype=np.float64)
+channelized = np.reshape(np.fromfile("channelized.bin", dtype=np.complex128), (M, -1))
+filt = np.fromfile("filter.bin", dtype=np.float64)
+fdata = np.reshape(np.fromfile("fftdata.bin", dtype=np.complex128), (M, -1))
+ffilt = np.reshape(np.fromfile("fftfilt.bin", dtype=np.complex128), (M, -1))
+
+fs = 10e3
+fig, axs = plt.subplots(3, 2)
+axs[0, 0].plot(filt)
+axs[0, 0].set_title("Filter Taps")
+tin = np.arange(len(indata)) / fs
+tout = np.arange(len(outdata)) / fs
+w, h = signal.freqz(filt, 1, fs=10e3)
+axs[1, 0].plot(w, 20*np.log10(np.abs(h)))
+axs[1, 0].plot([100, 100], [-60, 0], "r:")
+axs[1, 0].set_title("Filter Frequency Response")
+axs[1, 0].set_xlabel("Frequency (Hz)")
+axs[1, 0].set_ylabel("Magnitude (dB)")
+
+pchan = 1
+tds = np.arange(channelized.shape[1]) / fs * M
+axs[0, 1].plot(tin, indata, label="Input", color="dimgray", alpha=0.3)
+axs[0, 1].plot(tds, np.real(channelized[pchan, :]) / channelized.shape[1], label=f"Channel {pchan} (real)")
+axs[0, 1].plot(tds, np.imag(channelized[pchan, :]) / channelized.shape[1], label=f"Channel {pchan} (imag)")
+axs[0, 1].set_title("Input & Output")
+axs[0, 1].set_xlabel("Time (s)")
+axs[0, 1].set_ylabel("Amplitude")
+axs[0, 1].legend()
+
+#  axs[1, 1].pcolormesh(np.abs(ffilt))
+#  axs[1, 1].set_title("Filter Decomposition")
+#  axs[1, 1].set_xlabel("Frequency Index")
+#  axs[1, 1].set_ylabel("Polyphase Channel")
+
+axs[2, 1].pcolormesh(np.abs(fdata))
+axs[2, 1].set_title("FFT of Polyphase Data")
+axs[2, 1].set_xlabel("Frequency Index")
+axs[2, 1].set_ylabel("Polyphase Channel")
+
+axs[2, 0].pcolormesh(np.abs(channelized))
+axs[2, 0].set_title("Channelized Output")
+axs[2, 0].set_ylabel("Channel")
+axs[2, 0].set_xlabel("Time Sample")
+
+f, t, p = signal.stft(indata, fs=10e3, nperseg=8)
+axs[1, 1].pcolormesh(t, f, np.abs(p))
+axs[1, 1].set_xlabel("Time")
+axs[1, 1].set_ylabel("Frequency")
+
+plt.tight_layout()
+
+plt.show()
---
+++ c/udft.c
@@ -0,0 +1,208 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  osconv.c
+ *
+ *    Description:  Testing overlap/save with FFTW in C to get the basic right before
+ *                  using in the channelizer.
+ *
+ *        Version:  1.0
+ *        Created:  05/23/2022 07:32:05 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+#include <complex.h>
+#include <fftw3.h>
+#include <tgmath.h>
+#include <string.h>
+
+double coarse_gaussian() {
+  double out = 0;
+  for (int m = 0; m < 12; m++) {
+    out += (double) (rand() % 100);
+  }
+  out /= 100.0;
+  out -= 6.0;
+}
+
+int main(int argc, char **argv) {
+  // Variables
+  const int M = 4; // Downsample factor
+  const int Nfull = 256 * M;  // Total number of samples
+  const int Nds = Nfull / M;
+  const int Nfilt = 16 * M + 1;
+  const int Nfilt_half = (Nfilt - 1) >> 1;
+  const int Noutdelay = Nfilt_half / M;
+  const int Nfill = Nfull - Nfilt + 1;
+  const double pi = acosf(-1);
+  const double Fs = 10e3;
+  const double Ts = 1.0 / Fs;
+  const double Tfull = Nfull * Ts;
+  const double fc = 2600;
+  const double fcutoff = Fs / (2 * M);
+  const int nrows = M;
+  const int ncols = Nds;
+  const int ncols_fft = ((Nfull / M) / 2 + 1);
+  const int Nfft = ncols_fft * M;
+  srand(time(NULL));
+
+  // Arrays
+  double *full_in = (double *) fftw_malloc(sizeof(double) * Nfull);
+  double filt[Nfilt];
+  double *filt_full = (double *) fftw_malloc(sizeof(double) * Nfull);
+  fftw_complex *fft_in = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * Nfft);
+  fftw_complex *fft_filt = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * Nfft);
+  double *conv_out = (double *) fftw_malloc(sizeof(double) * Nfull);
+  fftw_complex *fft_mult = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * Nfft);
+  fftw_complex *udft = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * Nfull);
+
+  // Make filter
+  int outidx = 0;
+  double filt_sum = 0;
+  for (int m = -Nfilt_half; m <= Nfilt_half; m++) {
+    // Sinc low pass filter at twice the SOI freq
+    if (m == 0) {
+      filt[outidx] = 2 * fcutoff / Fs;
+    } else {
+      filt[outidx] = sin(2 * pi * fcutoff * m / Fs) / (m * pi);
+    }
+
+    // Hamming window and normalization
+    filt[outidx] *= (0.54 - 0.46 * cos(2 * pi * outidx / Nfilt));
+    filt_sum += filt[outidx++];
+  }
+  for (int m = 0; m < Nfilt; m++) filt[m] /= filt_sum;  // Normalize the filter
+
+  // Make input sinusoid
+  for (int m = 0; m < Nfull; m++) {
+    if (m < Nfill) {
+      /* full_in[m] = cos(2 * pi * fc * m * Ts) + coarse_gaussian() / 10.0; */
+      full_in[m] = sin(2 * pi * (2 * Fs / Tfull * pow(Ts * m, 2) / 2)) + coarse_gaussian() / 10.0;
+    } else {
+      full_in[m] = 0;
+    }
+  }
+
+  // Filter polyphase decomposition
+  for (int n = 0; n < ncols; n++) {
+    for (int rho = 0; rho < M; rho++) {
+      int inidx = n * M - rho;
+      int outidx = rho * ncols + n;
+      if (inidx < 0 || inidx >= Nfilt) {
+        filt_full[outidx] = 0;
+      } else {
+        filt_full[outidx] = filt[inidx];
+      }
+      outidx++;
+    }
+  }
+
+  // Data polyphase FFT
+  int n_size[] = {ncols};
+  int rank = 1;
+  int howmany = M;
+  int idist = 1;
+  int odist = ncols_fft;
+  int istride = M;
+  int ostride = 1;
+  int *inembed = NULL;
+  int *onembed = NULL;
+  fftw_plan psig = fftw_plan_many_dft_r2c(rank, n_size, howmany, full_in, inembed, istride, idist, fft_in,
+                                            onembed, ostride, odist, FFTW_ESTIMATE);
+
+  // Filter FFT
+  n_size[0] = ncols;
+  rank = 1;
+  howmany = M;
+  idist = ncols;
+  odist = ncols_fft;
+  istride = 1;
+  ostride = 1;
+  inembed = NULL;
+  onembed = NULL;
+  fftw_plan pfilt = fftw_plan_many_dft_r2c(rank, n_size, howmany, filt_full, inembed, istride, idist, fft_filt,
+                                             onembed, ostride, odist, FFTW_ESTIMATE);
+  fftw_execute(pfilt);
+  
+  // IFFT plan
+  n_size[0] = ncols;  // LOGICAL size, not PHYSICAL size of output array
+  rank = 1;
+  howmany = M;
+  idist = ncols_fft;
+  odist = ncols;
+  istride = 1;
+  ostride = 1;
+  inembed = NULL;
+  onembed = NULL;
+  fftw_plan pinv = fftw_plan_many_dft_c2r(rank, n_size, howmany, fft_mult, inembed, istride, idist, conv_out,
+                                            onembed, ostride, odist, FFTW_ESTIMATE);
+
+  // Forward FFT
+  fftw_execute(psig);
+
+  // Compute circular convolution
+  for (int m = 0; m < Nfft; m++) {
+    fft_mult[m] = fft_filt[m] * fft_in[m];
+  }
+  fftw_execute(pinv);
+
+  // Perform DFT down columns to get channelized output
+  n_size[0] = M;
+  rank = 1;
+  howmany = ncols;
+  idist = 1;
+  odist = 1;
+  istride = ncols;
+  ostride = ncols;
+  inembed = NULL;
+  onembed = NULL;
+  fftw_plan pudft = fftw_plan_many_dft_r2c(rank, n_size, howmany, conv_out, inembed, istride, idist, udft,
+                                             onembed, ostride, odist, FFTW_ESTIMATE);
+  fftw_execute(pudft);
+
+
+  // Write outputs
+  FILE* dout = fopen("filtered.bin", "wb");
+  fwrite(&conv_out[0], sizeof(double), Nfull, dout);
+  fclose(dout);
+  FILE *din = fopen("input.bin", "wb");
+  fwrite(&full_in[0], sizeof(double), Nfull, din);
+  fclose(din);
+  FILE *f_filt = fopen("filter.bin", "wb");
+  fwrite(&filt[0], sizeof(double), Nfilt, f_filt);
+  fclose(f_filt);
+  FILE* duout = fopen("channelized.bin", "wb");
+  fwrite(&udft[0], sizeof(fftw_complex), Nfull, duout);
+  fclose(duout);
+  FILE* ddata = fopen("fftdata.bin", "wb");
+  fwrite(&fft_in[0], sizeof(fftw_complex), Nfft, ddata);
+  fclose(ddata);
+  FILE* dfilt = fopen("fftfilt.bin", "wb");
+  fwrite(&fft_filt[0], sizeof(fftw_complex), Nfft, dfilt);
+  fclose(dfilt);
+
+  // Free memory
+  fftw_free(full_in);
+  fftw_free(filt_full);
+  fftw_free(udft);
+  fftw_free(fft_in);
+  fftw_free(fft_filt);
+  fftw_free(conv_out);
+  fftw_free(fft_mult);
+  fftw_destroy_plan(pfilt);
+  fftw_destroy_plan(psig);
+  fftw_destroy_plan(pinv);
+  fftw_destroy_plan(pudft);
+  fftw_cleanup();
+}
+
+
+
