2b2549e0449c37f72936c5f494bb888853363c8b
Eric Carlsen <ecarlsen@icr-team.com>
2022-07-28 08:21:51 -0600

    Better test script

--- pybind11/test_all.py
+++ pybind11/test_all.py
@@ -6,51 +6,35 @@ import iteration_mod as i_mod
 import example
 
 a = np.random.normal(0, 1, (3, 4)).astype('f')
-print("")
-print("Testing 'pet':")
-print("--------------")
 p = pet.Pet("Cassie")
-print(p.getName())
+assert "Cassie" == p.getName(), "Pet name incorrect"
 p.setName("Ginger")
-print(p.getName())
-print("Testing a * (2 + 1j)")
-print(f"a = {a}")
-print(p.nump(a))
+assert "Ginger" == p.getName(), "Pet name incorrect"
+a_t = a * (2 + 1j)
+a_c = a + 0j
+a_m = p.nump(a_c)
+assert np.allclose(a_t, a_m), "Pet.nump incorrect"
 
-print("")
-print("Testing 'iteration_mod':")
-print("------------------------")
-print(a*2)
-print(i_mod.nump(a))
+a_t = a.astype('d') * 2
+a_m = i_mod.nump(a.astype('d'))
+assert np.allclose(a_t, a_m), "iteration_mod.nump incorrect"
 
 b = np.random.randn(25)
 c = np.random.randn(25)
 d = np.zeros(25)
 e = i_mod.add_arrays(b, c, d)
-print("")
-print("i_mod.add_arrays:")
-#  print(f"in1: {b}")
-#  print(f"in2: {c}")
-#  print(f"in1 + in2: {b + c}")
-#  print(f"i_mod.add_arrays(in1, in2): {d}")
-print(f"sum(|delta|) = {np.sum(np.abs(d - b - c))}")
-print(f"sum(|delta|) = {np.sum(np.abs(d - e))}")
+assert np.allclose(b + c, d), "iteration_mod.add_arrays correct filling in last argument"
+assert np.allclose(b + c, e), "iteration_mod.add_arrays correct returning argument"
+assert example.add1(10, 20) == 30, "example.add1(10, 20) producing right result"
+assert example.add1(j=20) == 21, "example.add1(j=20) producing right result"
+assert example.add1(i=20) == 22, "example.add1(i=20) producing right result"
+assert example.add2(10, 20) == 30, "example.add2(10, 20) producing right result"
+assert example.add2(j=20) == 21, "example.add2(j=20) producing right result"
+assert example.add2(i=20) == 22, "example.add2(i=20) producing right result"
 
-print("")
-print("Testing 'example':")
-print("------------------")
-print(example.add1(10, 20))
-print(example.add1(j=20))
-print(example.add1(i=20))
-print(example.add2(10, 20))
-print(example.add2(j=20))
-print(example.add2(i=20))
-print("")
+f = np.random.normal(0, 1, (3, 4)) + 1j * np.random.normal(0, 1, (3, 4))
+f_t = f * (2 + 2j)
+f_m = i_mod.cnump(f)
+assert np.allclose(f_t, f_m), "iteration_mod.cnump produces correct output"
 
-f = np.random.normal(0, 1, (3, 4)).astype('f') + 1j * np.random.normal(0, 1, (3, 4)).astype('f')
-print("")
-print("Testing 'iteration_mod cnump':")
-print("------------------------")
-print(f * (2 + 2j))
-print(i_mod.cnump(f))
-
+print("All tests pased")
